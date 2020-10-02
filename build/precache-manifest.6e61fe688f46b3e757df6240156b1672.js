f72c5c5cfd83553d810219fa04ca9436c9d94132
Eliza Regas <elizaregas@gmail.com>
2020-10-02 15:08:40 -0400

    initial commit

---
+++ build/precache-manifest.6e61fe688f46b3e757df6240156b1672.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c91bf3ccf53b6e38e5001013f096103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e9bc6313dd91c1f4d2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "daf4b4cee3534185cbb2",
+    "url": "/static/js/2.7fc91e50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7fc91e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e9bc6313dd91c1f4d2",
+    "url": "/static/js/main.56fa502c.chunk.js"
+  },
+  {
+    "revision": "3e588b18b9ead54ebd62",
+    "url": "/static/js/runtime-main.bd018e83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
