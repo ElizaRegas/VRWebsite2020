7693898b45dc4ebc2732e9a67ca996dcbb812321
Eliza Regas <elizaregas@gmail.com>
2020-11-08 12:12:26 -0500

    adding items to portfolio

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,35 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import buddha from "../images/buddha.jpg";
 import clean from "../images/clean2-icon-1.png";
+import stem from "../images/shes-in-stem.jpg";
+import foodie from "../images/foodie-finder.jpg";
 import "../App.css";
 
-class PortfolioItems extends Component {
-  render() {
-    return (
-      <Carousel>
-        <div className="container portfolioPic">
-          <img src={clean} alt="clean titans application" />
+const PortfolioItems = () => {
+  return (
+    <Carousel>
+      <div className="container portfolioPic">
+        <img src={clean} alt="clean titans application" />
+        <div className="portfolioCard">
+          <p className="portfolioTypeText">Application/Team Project</p>
+          <p className="portfolioTitleText">Clean Titans</p>
+          <p className="portfolioBodyText">
+            Clean Titans 2.0 is the current version of an application designed
+            to teach children to responsibly do their chores. This was completed
+            as my final project while attending the Coding Boot Camp at GA Tech.
+          </p>
           <p className="legend">Legend 1</p>
-          <div className="portfolioCard">
-            <p className="portfolioTitleText">
-            Application/Team Project
-            </p>
-          </div>
         </div>
-        <div className="container portfolioPic">
-          <img src={buddha} alt="buddha pic" />
-          <p className="legend">Legend 2</p>
+      </div>
+
+      <div className="container portfolioPic">
+        <img src={stem} alt="stem project" />
+        <div className="portfolioCard">
+          <p className="portfolioTypeText">Hackathon Project</p>
+          <p className="portfolioTitleText">She's In STEM</p>
+          <p className="portfolioBodyText">
+            She's in STEM was a project on which I worked while attending the
+            Women Who Code ATL Annual Hackathon in 2017.
+          </p>
+          <p className="legend">Javascript, Materialize, Firebase</p>
         </div>
-        <div className="container portfolioPic">
-          <img src={buddha} alt="buddha pic" />
-          <p className="legend">Legend 3</p>
+      </div>
+
+      <div className="container portfolioPic">
+        <img src={foodie} alt="foodie application" />
+        <div className="portfolioCard">
+          <p className="portfolioTypeText">Application</p>
+          <p className="portfolioTitleText">Foodie Finder</p>
+          <p className="portfolioBodyText">
+            Foodie Finder is a friend finding application in which the user
+            answers questions and is matched with people who enjoy similar
+            foods.
+          </p>
+          <p className="legend">Javascript, Node, Express</p>
         </div>
-      </Carousel>
-    );
-  }
-}
+      </div>
+    </Carousel>
+  );
+};
 
 export default PortfolioItems;
 
